9b023dfa37c1a02193db94ce22847c69d8bb3a2f
maciejg <maciej.gora01@gmail.com>
2019-04-07 10:43:34 +0200

    Network Player Added
    Added NetworkManager. Remember to set player on it in "spawn info". Player needs the network identity to can be set to network manager.

---
+++ AM multiplayer/Assets/Scripts/MynetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MynetworkManager : NetworkManager {
+    [SerializeField] GameObject mPlayerPrefab;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
+        //base.OnServerAddPlayer(conn, playerControllerId);
+        GameObject player = (GameObject)Instantiate(mPlayerPrefab, Vector3.zero, Quaternion.identity);
+        //player.GetComponent<Player>().color =
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    public void mStartHost() {
+
+    }
+}
